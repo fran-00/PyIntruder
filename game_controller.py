f8434590f59222f1cdb8bb941608ed8eef157a26
fran-00 <fran.mf011@gmail.com>
2023-02-15 21:01:03 +0100

    change signal reception print data to provide more information

--- game_controller.py
+++ game_controller.py
@@ -16,15 +16,15 @@ class GameController(QObject):
     @pyqtSlot(str)
     def on_model_signal(self, data):
         # Process data received from the model
-        print("I'm receiving a signal from MODEL!")
-
+        print("My name is CONTROLLER and I'm receiving a signal from MODEL!")
         # Send data to the view
         self.view_signal.emit(data)
 
     @pyqtSlot(str)
     def on_view_signal(self, data):
         # Process data received from the view
-        print("I'm receiving a signal from VIEW!")
+        print("My name is CONTROLLER and I'm receiving a signal from VIEW!")
+        print(data)
 
         # Send data to the model
         self.model_signal.emit(data)
