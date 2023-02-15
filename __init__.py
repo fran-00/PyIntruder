c544161c1f5a3da59d3c470dbbfe579cd6423caf
fran-00 <fran.mf011@gmail.com>
2023-02-15 19:40:07 +0100

    update import from PyQt5 to PyQt6

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
 
 from game import Game
 from game_view import GameView
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     game_view.show()
 
     sys.exit(app.exec_())
+
