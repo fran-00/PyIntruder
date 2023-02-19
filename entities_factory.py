00783931224f4459fe2e13f51a9c646c5479331a
fran-00 <fran.mf011@gmail.com>
2023-02-19 09:32:50 +0100

    add "ats" consumable to new ItemsFactory class

--- entities_factory.py
+++ entities_factory.py
@@ -16,11 +16,14 @@ with open('items_data.json') as f:
     items_data = json.load(f)
 
 
-ats = Consumable(
-    "ATS",
-    items_data["consumables"]["Advanced Tea Substitute"],
-    random.randint(310, 340),
-    70
-)
+class ItemsFactory:
+    def __init__(self):
+        self.ats = Consumable(
+            "ATS",
+            items_data["consumables"]["Advanced Tea Substitute"],
+            random.randint(310, 340),
+            70
+        )
 
 
+print(ItemsFactory().ats.description)
