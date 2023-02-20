be12ccc4ae024fef5ee5d0f20d6bc12921a887da
fran-00 <fran.mf011@gmail.com>
2023-02-20 09:17:42 +0100

    add attribute level to Weapon class using super()

--- entities.py
+++ entities.py
@@ -20,8 +20,8 @@ class Item(Entity):
 
 class Weapon(Item):
     
-    def __init__(self, name, description, value, damage):
-        super().__init__(name, description, value, damage)
+    def __init__(self, name, description, level, value, damage):
+        super().__init__(name, description, level, value, damage)
         self.damage = damage
         
     def __str__(self):
