f52b3ab3c4415a5dd3416a70a1adcbb776d1a3b9
fran-00 <fran.mf011@gmail.com>
2023-02-26 16:03:59 +0100

    add more comments

--- game_controller.py
+++ game_controller.py
@@ -5,15 +5,30 @@ class GameController(QObject):
     controller_signal_to_view = pyqtSignal(str)
 
     def __init__(self, game_view, game_model):
+        '''
+        CONNECTS SLOTS AND SIGNALS:
+        - CONTROLLER gets game response from MODEL
+        - CONTROLLER sends game response to VIEW
+        - CONTROLLER gets user action from VIEW
+        - CONTROLLER send user action to MODEL
+        '''
         super().__init__()
         
-        # Connect model signals to controller slots
+        # Connect MODEL signals to CONTROLLER slots to get game
+        # response and send it to VIEW via on_model_signal method
         game_model.model_signal_to_controller.connect(self.on_model_signal)
-        # Connect controller signals to model slots
-        self.controller_signal_to_model.connect(game_model.handle_inbound_signal)
-        # Connect view signals to controller slots
+        
+        # Connect VIEW signals to CONTROLLER slots to get user
+        # input and send it to MODEL via on_view_signal method
         game_view.view_signal_to_controller.connect(self.on_view_signal)
-        # Connect controller signals to view slots
+        
+        # Connect CONTROLLER signals to MODEL slots to send user action,
+        # then handle_inbound_signal method will send them to gameloop
+        self.controller_signal_to_model.connect(game_model.handle_inbound_signal)
+        
+        # FIXME: this doesn't work
+        # Connect CONTROLLER signals to VIEW slots to send game response
+        # then ??? will show them and wait for new user input 
         self.controller_signal_to_view.connect(game_view.handle_game_response)
 
 
