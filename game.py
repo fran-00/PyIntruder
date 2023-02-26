918e4da30220596d124996a0773060d8c160a794
fran-00 <fran.mf011@gmail.com>
2023-02-26 18:46:36 +0100

    update handle_inbound/outbound_signal to print better info

--- game.py
+++ game.py
@@ -29,16 +29,15 @@ class Game(QObject):
     def handle_inbound_signal(self, user_action):
         ''' Slot that receives a string from controller as a signal '''
         
-        print(f"I'm MODEL and I got a signal from CONTROLLER: {user_action}")
+        print(f"MODEL: I got a signal from CONTROLLER with user action: {user_action}")
         self.action = user_action
-
 
     def handle_outbound_signal(self):
         ''' Takes a string an send it to controller as a signal '''
         # Test game response
         game_response = "HI"
         
-        print(f"I'm MODEL and I'm sending a signal to CONTROLLER: {game_response}")
+        print(f"MODEL: I'm sending a signal to CONTROLLER with game response: {game_response}")
         # Emits the signal that contains game response
         self.model_signal_to_controller.emit(game_response)
         
