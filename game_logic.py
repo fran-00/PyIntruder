b6e209b3898b4ee143c72189a63774b560143f9a
fran-00 <fran.mf011@gmail.com>
2023-04-19 23:19:26 +0200

    Add GameLogic class with a check_tile method: this is an implementation of the original Game class that prints an output based on the player's position and the presence of enemies as long as the player is alive

---
+++ game_logic.py
@@ -0,0 +1,29 @@
+import os
+import world
+from player import Player
+
+
+class GameLogic:
+    def __init__(self):
+        self.player = Player()
+        self.room = world.tile_at(self.player.x, self.player.y)
+        
+    def check_tile(self):
+        if self.player.is_alive():
+
+            if self.player.verbose and self.room.enemy is None:
+                return (f"\n***{self.room.name}***\n>{self.room.description}")
+            
+            elif (
+                self.player.verbose
+                and self.room.enemy.alive is True
+                or not self.player.verbose
+                and self.room.enemy is not None
+                and self.room.enemy.alive is True
+            ):
+                return (f"> {self.room.enemy.intro_alive}")
+            elif self.player.verbose and self.room.enemy.alive is False:
+                return (f"\n***{self.room.name}***\n>{self.room.enemy.intro_dead}")
+            elif not self.player.verbose and self.room.enemy is None:
+                return (f"\n***{self.room.name}***")
+            
