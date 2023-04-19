02af2d9acb6496c41b20b878a05abe3b91e462e2
fran-00 <fran.mf011@gmail.com>
2023-04-19 22:24:13 +0200

    Fix play method to use signals and event loop to comunicate with controller and view

--- game.py
+++ game.py
@@ -33,24 +33,11 @@ class Game(QObject):
         world.parse_world_dsl()
         player = Player()
  
-        while player.is_alive:
-            # Il model riceve l'azione dell'utente dalla view
-            user_action = None
-            while not user_action:
-                loop.processEvents()
-                if self.action:
-                    user_action = self.action
-                    self.action = None
-
-            # Il model processa l'azione dell'utente e genera una risposta
-            game_response = "I beg you pardon?"
-
-            # Il model invia la risposta alla view
+        while True:
+            game_response = self.get_game_response()
+            print(f"MODEL: Game response is: {game_response}")
             self.model_signal_to_controller.emit(game_response)
-
-            # Il model controlla se il giocatore è vivo e passa al prossimo turno
-            if not player.is_alive:
-                break
+            self.event_loop.exec()
 
     @pyqtSlot(str)
     def handle_inbound_signal(self, user_action):
