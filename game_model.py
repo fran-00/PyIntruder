2a4dd30af7af86914aea785c7dc6b7d7431081b9
fran-00 <fran.mf011@gmail.com>
2023-04-20 22:34:29 +0200

    Remove printout of what model does because it's no longer needed

--- game_model.py
+++ game_model.py
@@ -46,18 +46,11 @@ class GameModel(QObject):
     def handle_inbound_signal(self, user_action):
         ''' Slot that receives a string from controller as a signal '''
         self.action = user_action
-        
-        print(f"MODEL: I got a signal from CONTROLLER with user action: {user_action}")
-        
         self.event_loop.exit()
 
 
     def handle_outbound_signal(self, game_response):
         ''' Takes a string an send it to controller as a signal '''
-        
-        print(f"MODEL: I'm sending a signal to CONTROLLER with game response: {game_response}")
-        
-        # Emits the signal that contains game response
         self.model_signal_to_controller.emit(game_response)
 
 
