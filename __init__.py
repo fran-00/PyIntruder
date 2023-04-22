65920bc53e5523443b4c4b35876bb02389cb8645
fran-00 <fran.mf011@gmail.com>
2023-04-22 08:29:33 +0200

    Change game_view, game_controller and game_model variables names to view, controller and model to improve readability

--- __init__.py
+++ __init__.py
@@ -10,10 +10,10 @@ from engine.controller import GameController
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    game_view = GameView()
-    game_model = GameModel()
-    game_controller = GameController(game_view, game_model)
-    game_view.show()
+    view = GameView()
+    model = GameModel()
+    controller = GameController(view, model)
+    view.show()
 
     sys.exit(app.exec())
 
