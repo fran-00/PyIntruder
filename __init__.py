49bcbb2b622d4757408b1a26939630e52855a79e
fran-00 <fran.mf011@gmail.com>
2023-04-23 10:43:27 +0200

    Add world and player imports

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
+import world
 from engine.model import GameModel
 from engine.view import GameView
 from engine.controller import GameController
-    
+from entities.player import Player
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
