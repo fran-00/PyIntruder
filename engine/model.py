68560db05dab8a9fae9cc300a68be6840e91d235
fran-00 <fran.mf011@gmail.com>
2023-04-23 13:35:27 +0200

    Remove get_available_action, available_directions and action_adder methods because now their usage is handled by move method

--- engine/model.py
+++ engine/model.py
@@ -97,11 +97,6 @@ class GameModel(QObject):
         elif action in ['now']:
             return (f"This room is {self.player.x}, {self.player.y}")
 
-        # *** FORBIDDEN DIRECTIONS ***
-        elif (available_actions != ['n', 's', 'w', 'e'] and
-            action in ['n', 's', 'w', 'e'] and
-            self.room.enemy is not None and self.room.enemy.alive is True):
-            return ("You cannot leave while an enemy attacks you!")
 
     def move(self, room, player):
         if self.action == "n" and world.tile_at(room.x, room.y - 1):
@@ -113,40 +108,7 @@ class GameModel(QObject):
         elif self.action == "w" and world.tile_at(room.x - 1, room.y):
             player.move_west()
         else:
-            return("I beg you pardon?")
+            pass
 
 
-    def get_available_actions(self, room, player):
-        actions = OrderedDict()
-
-        if room.enemy is None or room.enemy.alive is False:
-            self.available_directions(room, actions, player)
-        if room.enemy is not None and room.enemy.alive is True:
-            self.action_adder(actions, 'a', player.attack, "attack")
-        if room.enemy is not None and room.enemy.alive is True:
-            self.action_adder(actions, 'c', player.cast_curse, "cast curse")
-        if room.enemy is not None and room.enemy.alive is True:
-            self.action_adder(actions, 'r', player.run, "run")
-        if room.talker:
-            self.action_adder(actions, 't', player.check_dialogue, "talk")
-        if player.hp < player.max_hp:
-            self.action_adder(actions, 'h', player.heal, "heal")
-        return actions
-
-
-    def available_directions(self, room, actions, player):
-        if world.tile_at(room.x, room.y - 1):
-            self.action_adder(actions, 'n', player.move_north, "north")
-        if world.tile_at(room.x, room.y + 1):
-            self.action_adder(actions, 's', player.move_south, "south")
-        if world.tile_at(room.x + 1, room.y):
-            self.action_adder(actions, 'e', player.move_east, "east")
-        if world.tile_at(room.x - 1, room.y):
-            self.action_adder(actions, 'w', player.move_west, "west")
-
-
-    def action_adder(self, action_dict, hotkey, action, name):
-        action_dict[hotkey.lower()] = action
-        action_dict[hotkey.upper()] = action
-        print(f"| {hotkey}: {name}")
 
