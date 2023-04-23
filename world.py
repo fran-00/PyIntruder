dd7e299914642c51444f74064a6675d846189253
fran-00 <fran.mf011@gmail.com>
2023-04-23 14:16:19 +0200

    Change any print function to return in modify_player method

--- world.py
+++ world.py
@@ -30,27 +30,25 @@ class MapTile:
             confusion_chance = random.randint(1, 20)
             if confusion_chance >= 19 and self.enemy.damage < self.enemy.hp:                       #in questo modo non può uccidersi da solo
                 self.enemy.hp -= self.enemy.damage
-                print(f"> {self.enemy.name} is confused!")
-                print(f"> It hurts itself in its confusion! (Deals {self.enemy.damage} DMG and has {self.enemy.hp} HP remaining.)")
+                return (f"{self.enemy.name} is confused!\nIt hurts itself in its confusion! (Deals {self.enemy.damage} DMG and has {self.enemy.hp} HP remaining.)")
             elif confusion_chance in [17, 18]:
-                print(f"> {self.enemy.name} is confused!")
-                print(f"> {self.enemy.name} misses the shot!")
+                return (f"{self.enemy.name} is confused!\n{self.enemy.name} misses the shot!")
             elif player.base_defence == 0:
                 player.hp = player.hp - self.enemy.damage
                 if player.hp > 0:
-                    print(f"> {self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you have {player.hp} HP remaining...")
+                    return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you have {player.hp} HP remaining...")
                 elif player.hp <= 0:
-                    print(f"> {self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you died...")
+                    return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you died...")
             elif player.base_defence > 0:
                 damage_reduction = 5 * player.base_defence
                 if damage_reduction < self.enemy.damage:
                     player.hp = player.hp - (self.enemy.damage - damage_reduction)
                     if player.hp > 0:
-                        print(f"> {self.enemy.name} inflicts {self.enemy.damage} DMG to you, but your armor reduce the damage by {damage_reduction}, so you have {player.hp} HP remaining...")
+                        return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you, but your armor reduce the damage by {damage_reduction}, so you have {player.hp} HP remaining...")
                     elif player.hp <= 0:
-                        print(f"> {self.enemy.name} inflicts {self.enemy.damage} DMG to you, your armor reduce the damage by {damage_reduction} but you died anyway...")
+                        return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you, your armor reduce the damage by {damage_reduction} but you died anyway...")
                 else:
-                    print(f"> {self.enemy.name} tries to inflict {self.enemy.damage} DMG to you, but your armor absorbes {damage_reduction} DMG, so it did nothing to you and you still have {player.hp} HP remaining...")
+                    return (f"{self.enemy.name} tries to inflict {self.enemy.damage} DMG to you, but your armor absorbes {damage_reduction} DMG, so it did nothing to you and you still have {player.hp} HP remaining...")
         else:
             return
 
