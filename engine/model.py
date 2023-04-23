9a96a89de325c29b9b0643903a3147b95e4d440d
fran-00 <fran.mf011@gmail.com>
2023-04-23 14:14:23 +0200

    Remove verbose variable usage and try to call modify_player from world to test fight mechanics

--- engine/model.py
+++ engine/model.py
@@ -63,17 +63,11 @@ class GameModel(QObject):
         self.room = world.tile_at(self.player.x, self.player.y)
         if self.player.is_alive():
 
-            if self.player.verbose and self.room.enemy is None:
-                return (f"\n***{self.room.name}***\n>{self.room.description}")
-            
-            elif (
-                self.player.verbose
-                and self.room.enemy.alive is True
-                or not self.player.verbose
-                and self.room.enemy is not None
-                and self.room.enemy.alive is True
-            ):
-                return (f"{self.room.enemy.intro_alive}")
+            if self.room.enemy is None:
+                return (f"\n***{self.room.name}***\n> {self.room.description}")
+            elif self.room.enemy.alive:
+                self.room.modify_player(self.player)
+                return (f"{self.room.enemy.intro_alive}\n{self.room.modify_player(self.player)}")
             elif self.player.verbose and self.room.enemy.alive is False:
                 return (f"\n***{self.room.name}***\n>{self.room.enemy.intro_dead}")
             elif not self.player.verbose and self.room.enemy is None:
