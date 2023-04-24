6840d81402273ee358db0c5b103b997cb81c3ecf
fran-00 <fran.mf011@gmail.com>
2023-04-24 13:03:24 +0200

    remove player health check from check_tile method cause it will be handled elsewhere

--- engine/model.py
+++ engine/model.py
@@ -62,16 +62,14 @@ class GameModel(QObject):
 
     def check_tile(self):
         self.room = world.tile_at(self.player.x, self.player.y)
-        if self.player.is_alive():
-
-            if self.room.enemy is None:
-                return (f"\n***{self.room.name}***\n> {self.room.description}")
-            elif self.room.enemy.alive:
-                return (f"{self.room.enemy.intro_alive}")
-            elif self.player.verbose and self.room.enemy.alive is False:
-                return (f"\n***{self.room.name}***\n>{self.room.enemy.intro_dead}")
-            elif not self.player.verbose and self.room.enemy is None:
-                return (f"\n***{self.room.name}***")
+        if self.room.enemy is None:
+            return (f"\n***{self.room.name}***\n> {self.room.description}")
+        elif self.room.enemy.alive:
+            return (f"{self.room.enemy.intro_alive}")
+        elif self.player.verbose and self.room.enemy.alive is False:
+            return (f"\n***{self.room.name}***\n>{self.room.enemy.intro_dead}")
+        elif not self.player.verbose and self.room.enemy is None:
+            return (f"\n***{self.room.name}***")
     
             
     def choose_action(self, action=str):
