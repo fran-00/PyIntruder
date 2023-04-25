23b8fa85f7af94f2a73e4a91d52cfeb6271a7b53
fran-00 <fran.mf011@gmail.com>
2023-04-25 08:59:49 +0200

    Refactor modify_player method on MapTile class to comply DRY principle

--- world/tiles.py
+++ world/tiles.py
@@ -31,30 +31,44 @@ class MapTile:
 
 
     def modify_player(self, player):
-        if self.enemy is not None and self.enemy.alive is True:
-            confusion_chance = random.randint(1, 20)
-            if confusion_chance >= 19 and self.enemy.damage < self.enemy.hp:                       #in questo modo non può uccidersi da solo
-                self.enemy.hp -= self.enemy.damage
-                return (f"{self.enemy.name} is confused!\nIt hurts itself in its confusion! (Deals {self.enemy.damage} DMG and has {self.enemy.hp} HP remaining.)")
-            elif confusion_chance in [17, 18]:
-                return (f"{self.enemy.name} is confused!\n{self.enemy.name} misses the shot!")
-            elif player.base_defence == 0:
-                player.hp = player.hp - self.enemy.damage
-                if player.hp > 0:
-                    return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you have {player.hp} HP remaining...")
-                elif player.hp <= 0:
-                    return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you. Oh shit, you died...")
-            elif player.base_defence > 0:
-                damage_reduction = 5 * player.base_defence
-                if damage_reduction < self.enemy.damage:
-                    player.hp = player.hp - (self.enemy.damage - damage_reduction)
-                    if player.hp > 0:
-                        return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you, but your armor reduce the damage by {damage_reduction}, so you have {player.hp} HP remaining...")
-                    elif player.hp <= 0:
-                        return (f"{self.enemy.name} inflicts {self.enemy.damage} DMG to you, your armor reduce the damage by {damage_reduction} but you died anyway...")
-                else:
-                    return (f"{self.enemy.name} tries to inflict {self.enemy.damage} DMG to you, but your armor absorbes {damage_reduction} DMG, so it did nothing to you and you still have {player.hp} HP remaining...")
-        else:
+        if self.enemy is None or not self.enemy.alive:
             return
+        confusion_chance = random.randint(1, 20)
+        if confusion_chance >= 19 and self.enemy.damage < self.enemy.hp:
+            self.enemy.hp -= self.enemy.damage
+            return (
+                f"{self.enemy.name} is confused!\n"
+                f"It hurts itself in its confusion! "
+                f"(Deals {self.enemy.damage} DMG and has {self.enemy.hp} HP remaining.)"
+            )
+
+        if confusion_chance in [17, 18]:
+            return f"{self.enemy.name} is confused!\n{self.enemy.name} misses the shot!"
+        if player.base_defence == 0:
+            return self.damage_player(player, self.enemy.damage, None)
+        damage_reduction = 5 * player.base_defence
+        return self.damage_player(player, self.enemy.damage, damage_reduction)
+
+    def damage_player(self, player, damage, damage_reduction):
+        if damage_reduction is not None:
+            damage -= damage_reduction
+
+        player.hp -= damage
+
+        if player.hp <= 0:
+            return (
+                f"{self.enemy.name} inflicts {damage} DMG to you, "
+                f"your armor reduce the damage by {damage_reduction or 0} but you died anyway..."
+            )
+        if damage_reduction is not None:
+            return (
+                f"{self.enemy.name} inflicts {damage} DMG to you, "
+                f"but your armor reduce the damage by {damage_reduction}, "
+                f"so you have {player.hp} HP remaining..."
+            )
+        return (
+            f"{self.enemy.name} inflicts {damage} DMG to you. "
+            f"Oh shit, you have {player.hp} HP remaining..."
+        )
 
 
