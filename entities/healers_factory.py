d3f717b3173e642b0678b485b78c128e36f160ea
fran-00 <fran.mf011@gmail.com>
2023-04-27 23:10:14 +0200

    Add new HealersFactory to create instances from healer class

---
+++ entities/healers_factory.py
@@ -0,0 +1,27 @@
+import random, json
+
+from .entities_templates import Healer
+
+
+with open('entities_data/items_data.json') as f:
+    items_data = json.load(f)
+
+
+class HealersFactory:
+    
+    def __init__(self):
+        
+        self.ats = Healer(
+            "Advanced Tea Substitute",
+            items_data["healers"]["ats"],
+            2,
+            1,
+            3           
+        )       
+        self.gommo = Healer(
+            "Gommo",
+            items_data["healers"]["gommo"],
+            2,
+            1,
+            3           
+        )
