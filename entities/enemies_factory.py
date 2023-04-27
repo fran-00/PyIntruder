dd84b3095a8d446143e1ac868df7d6ff58bdaabd
fran-00 <fran.mf011@gmail.com>
2023-04-27 15:55:07 +0200

    Add new script with classes that create Enemy's instances of 5 different levels

---
+++ entities/enemies_factory.py
@@ -0,0 +1,119 @@
+from .entities_templates import Enemy
+import random, json
+
+
+with open('entities_data/enemies_data.json') as ef:
+    enemies_data = json.load(ef)
+
+
+class Lv1:
+    
+    def __init__(self):
+        self.gel_cube = Enemy(
+            "Gelatinous Cube",
+            enemies_data["gel cube"]["intro_alive"],
+            1,
+            20,
+            10,
+            True
+        )
+        self.squirrel = Enemy(
+            "Ravenous Squirrel",
+            enemies_data["squirrel"]["intro_alive"],
+            1,
+            30,
+            15,
+            True
+        )
+        self.helicopter = Enemy(
+            "Helicopter",
+            enemies_data["helicopter"]["intro_alive"],
+            1,
+            40,
+            20,
+            True
+        )
+
+
+class Lv2:
+    def __init__(self):
+        
+        self.hunter = Enemy(
+            "Mushroom Hunter",
+            enemies_data["hunter"]["intro_alive"],
+            2,
+            50,
+            25,
+            True
+        )
+        self.bug = Enemy(
+            "Bug in the Program",
+            enemies_data["bug"]["intro_alive"],
+            2,
+            60,
+            30,
+            True
+        )
+        self.eyes = Enemy(
+            "Floating Eyes",
+            enemies_data["eyes"]["intro_alive"],
+            3,
+            70,
+            35,
+            True
+        )
+
+
+class Lv3:
+    def __init__(self):
+        self.ants = Enemy(
+            "ants infected by a fungus",
+            enemies_data["ants"]["intro_alive"],
+            3,
+            80,
+            40,
+            True
+        )
+        self.trog = Enemy(
+            "Trog",
+            enemies_data["trog"]["intro_alive"],
+            3,
+            90,
+            45,
+            True
+        )
+        self.uncanny = Enemy(
+            "Uncanny Valley",
+            enemies_data["uncanny"]["intro_alive"],
+            4,
+            100,
+            50,
+            True
+        )
+
+
+class Lv4:
+    def __init__(self):
+
+        self.gnome = Enemy(
+            "Gnome armed with an ax",
+            enemies_data["gnome"]["intro_alive"],
+            4,
+            120,
+            60,
+            True
+        )
+
+
+class Lv5:
+    def __init__(self):
+
+        self.ostoyae = Enemy(
+            "Armillaria Ostoyae",
+            enemies_data["ostoyae"]["intro_alive"],
+            5,
+            150,
+            75,
+            True
+        )
+
