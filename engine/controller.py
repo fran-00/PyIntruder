8491feaac3b85611ad6f724d6abb67abff4e2fd4
fran-00 <fran.mf011@gmail.com>
2023-04-29 13:26:57 +0200

    Update script formatting

--- engine/controller.py
+++ engine/controller.py
@@ -6,8 +6,8 @@ class GameController(QObject):
     controller_signal_to_view = pyqtSignal(str)
 
     def __init__(self, view, model, thread):
-        """ 
-        Initialize the controller with the given view, model, and thread.
+        """Initialize the controller with the given view, model, and thread.
+
         Connects signals and slots to enable communication between the different components of the game:
         - The MODEL signals are connected to the CONTROLLER slots to get game responses, which are then sent to the VIEW via the `on_model_signal` method.
         - The VIEW signals are connected to the CONTROLLER slots to get user input, which is then sent to the MODEL via the `on_view_signal` method.
@@ -21,9 +21,10 @@ class GameController(QObject):
 
         Returns:
             None
+
         """
         super().__init__()
-        
+
         model.model_signal_to_controller.connect(self.on_model_signal)
         view.view_signal_to_controller.connect(self.on_view_signal)
         self.controller_signal_to_model.connect(model.handle_inbound_signal)
@@ -34,12 +35,15 @@ class GameController(QObject):
 
     @pyqtSlot(str)
     def on_model_signal(self, data):
-        ''' Process data received from the MODEL and send it to VIEW '''
+        """Process data received from the MODEL and send it to VIEW
+        
+        """
         self.controller_signal_to_view.emit(data)
 
     @pyqtSlot(str)
     def on_view_signal(self, data):
-        ''' Process data received from the VIEW and send it to MODEL '''
+        """ Process data received from the VIEW and send it to MODEL
+        
+        """
         self.controller_signal_to_model.emit(data)
-
 
