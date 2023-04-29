1eb7be4dad89f86beedd6d79cb55acb64386a6cc
fran-00 <fran.mf011@gmail.com>
2023-04-29 16:10:07 +0200

    Add new arguments to constructor in NonPlayableCharacter class and improve script formatting

--- entities/entities_templates.py
+++ entities/entities_templates.py
@@ -1,5 +1,5 @@
 class Entity:
-    
+
     def __init__(self, name, description, level):
         self.name = name
         self.description = description
@@ -10,7 +10,7 @@ class Entity:
 
 
 class Item(Entity):
-    
+
     def __init__(self, name, description, level, value):
         super().__init__(name, description, level)
         self.value = value
@@ -18,59 +18,62 @@ class Item(Entity):
 
 
 class Weapon(Item):
-    
+
     def __init__(self, name, description, level, value, damage):
         super().__init__(name, description, level, value)
         self.damage = damage
-        
+
     def __str__(self):
         return self.name, self.damage
 
 
 class Curse(Item):
-    
+
     def __init__(self, name, description, level, value, damage, mana_cost):
         super().__init__(name, description, level, value)
         self.damage = damage
         self.mana_cost = mana_cost
-    
+
     def __str__(self):
         return self.name, self.damage
 
 
 class Healer(Item):
-    
+
     def __init__(self, name, description, level, value, heal):
         super().__init__(name, description, level, value)
         self.heal = heal
 
 
 class ManaRecharger(Item):
-    
+
     def __init__(self, name, description, level, value, mr):
         super().__init__(name, description, level, value)
         self.mr = mr
 
 
 class Armor(Item):
-    
+
     def __init__(self, name, description, level, value, defence):
         super().__init__(name, description, level, value)
         self.defence = defence
 
 
 class NonPlayableCharacter(Entity):
-    
-    def __init__(self, name, description, level, gold, items):
+
+    def __init__(self, name, description, level, gold, items, hello, trade):
         super().__init__(name, description, level)
         self.gold = gold
         self.items = items
         self.inventory = self.sort_inventory(self.items)
+        self.hello = hello
+        self.trade = trade
 
     def sort_inventory(self, items):
         inventory = []
         for category in [Weapon, Curse, Healer, Armor]:
-            inventory.extend(sorted([item for item in items if isinstance(item, category)], key=lambda item: item.name.lower()))
+            inventory.extend(sorted([item for item in items if isinstance(
+                item, category)], key=lambda item: item.name.lower()))
         return inventory
 
 
@@ -87,5 +90,3 @@ class Enemy(Entity):
 
     def is_alive(self):
         return self.hp > 0
-
-
