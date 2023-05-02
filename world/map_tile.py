510d961d3b9aae1f79e29c374c0f39c6988c1897
fran-00 <fran.mf011@gmail.com>
2023-05-02 18:47:21 +0200

    Add dialogue and rename check if trading in MapTile class

--- world/map_tile.py
+++ world/map_tile.py
@@ -90,8 +90,11 @@ class MapTile:
         )
 
 
-    def choose_dialogue(self, *args):
+    def check_if_trading(self, *args):
         if args[1] == "trade":
             return "Buy, Sell or Quit?"
         elif args[1] == "talk":
             return "self.talker.hello"
+    
+    def dialogue(self, *args):
+        return "We are talking."
