164d9de988007477d33d07098347b38e8710308a
fran-00 <fran.mf011@gmail.com>
2023-05-03 09:49:32 +0200

    Remove any description attribute from Curse subclasses instances

--- entities/factories/curses_factory.py
+++ entities/factories/curses_factory.py
@@ -6,7 +6,6 @@ class CursesFactory:
     def __init__(self):
         self.red = Curse(
             "self-luminous red",
-            items_data["curses"]["self-luminous red"],
             1,
             15,
             20,
@@ -14,7 +13,6 @@ class CursesFactory:
         )
         self.hyperbolic_orange = Curse(
             "hyperbolic orange",
-            items_data["curses"]["hyperbolic orange"],
             1,
             15,
             20,
@@ -22,7 +20,6 @@ class CursesFactory:
         )
         self.stygian_blue = Curse(
             "stygian blue",
-            items_data["curses"]["stygian blue"],
             1,
             15,
             20,
@@ -30,7 +27,6 @@ class CursesFactory:
         )
         self.discourse = Curse(
             "Discourse on the Method of Rightly Conducting One's Reason and of Seeking Truth in the Sciences",
-            items_data["curses"]["method"],
             1,
             15,
             20,
@@ -38,7 +34,6 @@ class CursesFactory:
         )
         self.falsidical = Curse(
             "falsidical paradox",
-            items_data["curses"]["falsidical"],
             1,
             15,
             20,
@@ -46,7 +41,6 @@ class CursesFactory:
         )
         self.cluster_point = Curse(
             "cluster point",
-            items_data["curses"]["cluster point"],
             1,
             15,
             20,
@@ -54,7 +48,6 @@ class CursesFactory:
         )
         self.aleph = Curse(
             "Aleph Naught",
-            items_data["curses"]["aleph"],
             1,
             15,
             20,
@@ -62,7 +55,6 @@ class CursesFactory:
         )
         self.logistic_map = Curse(
             "Logistic Map",
-            items_data["curses"]["logistic map"],
             1,
             15,
             20,
@@ -70,7 +62,6 @@ class CursesFactory:
         )
         self.integral = Curse(
             "integral of a real multivariate function",
-            items_data["curses"]["integral"],
             1,
             15,
             20,
@@ -78,7 +69,6 @@ class CursesFactory:
         )
         self.veridical = Curse(
             "veridical paradox",
-            items_data["curses"]["veridical"],
             1,
             15,
             20,
@@ -86,7 +76,6 @@ class CursesFactory:
         )
         self.comic_sans = Curse(
             "Comic Sans",
-            items_data["curses"]["comic sans"],
             1,
             15,
             20,
@@ -94,7 +83,6 @@ class CursesFactory:
         )
         self.sep = Curse(
             "Somebody Else's Problem Field",
-            items_data["curses"]["sep field"],
             1,
             15,
             20,
@@ -102,7 +90,6 @@ class CursesFactory:
         )
         self.nimby = Curse(
             "not in my back yard",
-            items_data["curses"]["nimby"],
             1,
             15,
             20,
@@ -110,7 +97,6 @@ class CursesFactory:
         )
         self.choice = Curse(
             "Axiom of Choice",
-            items_data["curses"]["choice"],
             1,
             15,
             20,
@@ -118,7 +104,6 @@ class CursesFactory:
         )
         self.antinomy = Curse(
             "antinomy",
-            items_data["curses"]["antinomy"],
             1,
             15,
             20,
@@ -126,7 +111,6 @@ class CursesFactory:
         )
         self.continuum = Curse(
             "continuum hypothesis",
-            items_data["curses"]["continuum"],
             1,
             15,
             20,
@@ -134,7 +118,6 @@ class CursesFactory:
         )
         self.insult = Curse(
             "Ultimate Insult",
-            items_data["curses"]["insult"],
             1,
             15,
             20,
@@ -142,7 +125,6 @@ class CursesFactory:
         )
         self.briefcase = Curse(
             "Marcellus Wallace's briefcase",
-            items_data["curses"]["briefcase"],
             1,
             15,
             20,
@@ -150,7 +132,6 @@ class CursesFactory:
         )
         self.riemann = Curse(
             "Riemann zeta function",
-            items_data["curses"]["riemann"],
             1,
             15,
             20,
