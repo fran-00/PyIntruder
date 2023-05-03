e450fe793ce5efebe34b7a557de142c845477b6a
fran-00 <fran.mf011@gmail.com>
2023-05-03 09:28:40 +0200

    Remove description attribute from any Weapon istance

--- entities/factories/weapons_factory.py
+++ entities/factories/weapons_factory.py
@@ -13,35 +13,30 @@ class WeaponsFactory:
         
         self.manuport = Weapon(
             "manuport",
-            items_data["weapons"]["manuport"],
             1,
             15,
             20,
         )
         self.sheet = Weapon(
             "polarized sheet",
-            items_data["weapons"]["polarized sheet"],
             1,
             15,
             20,
         )
         self.polyhedron = Weapon(
             "sharp polyhedron",
-            items_data["weapons"]["sharp polyhedron"],
             1,
             15,
             20,
         )
         self.armored_manuport = Weapon(
             "armored manuport",
-            items_data["weapons"]["armored manuport"],
             1,
             15,
             20,
         )
         self.polygon = Weapon(
             "transparent polygon",
-            items_data["weapons"]["transparent polygon"],
             1,
             15,
             20,
@@ -49,63 +44,54 @@ class WeaponsFactory:
         
         self.wire = Weapon(
             "wireless wire",
-            items_data["weapons"]["wireless wire"],
             1,
             15,
             20,
         )
         self.branch = Weapon(
             "armored brench",
-            items_data["weapons"]["armored brench"],
             1,
             15,
             20,
         )
         self.poly = Weapon(
             "n-dimensional polytope",
-            items_data["weapons"]["polytope"],
             1,
             15,
             20,
         )
         self.device = Weapon(
             "Aperture Science Handheld Portal Device",
-            items_data["weapons"]["ashpd"],
             1,
             15,
             20,
         )
         self.deliverance = Weapon(
             "Deliverance by Tediore (Shotgun)",
-            items_data["weapons"]["deliverance"],
             1,
             15,
             20,
         )
         self.tesseract = Weapon(
             "name",
-            items_data["weapons"]["tesseract"],
             1,
             15,
             20,
         )
         self.chuck = Weapon(
             "Chuck the plant",
-            items_data["weapons"]["chuck"],
             1,
             15,
             20,
         )
         self.attractor = Weapon(
             "great attractor",
-            items_data["weapons"]["great attractor"],
             1,
             15,
             20,
         )
         self.cube = Weapon(
             "The Soul Cube",
-            items_data["weapons"]["soul Cube"],
             1,
             15,
             20,
