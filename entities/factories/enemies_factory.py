3ec7b272aa66c755463b32b9086381a99e107229
fran-00 <fran.mf011@gmail.com>
2023-05-03 08:17:55 +0200

    Remove alive attribute from any Enemy's instance in EnemiesFactory's constructor

--- entities/factories/enemies_factory.py
+++ entities/factories/enemies_factory.py
@@ -14,24 +14,21 @@ class EnemiesFactory:
             enemies_data["gel cube"]["intro_alive"],
             1,
             20,
-            10,
-            True
+            10
         )
         self.squirrel = Enemy(
             "Ravenous Squirrel",
             enemies_data["squirrel"]["intro_alive"],
             1,
             30,
-            15,
-            True
+            15
         )
         self.helicopter = Enemy(
             "Helicopter",
             enemies_data["helicopter"]["intro_alive"],
             1,
             40,
-            20,
-            True
+            20
         )
 
         # Level 2
@@ -40,24 +37,21 @@ class EnemiesFactory:
             enemies_data["hunter"]["intro_alive"],
             2,
             50,
-            25,
-            True
+            25
         )
         self.bug = Enemy(
             "Bug in the Program",
             enemies_data["bug"]["intro_alive"],
             2,
             60,
-            30,
-            True
+            30
         )
         self.eyes = Enemy(
             "Floating Eyes",
             enemies_data["eyes"]["intro_alive"],
             3,
             70,
-            35,
-            True
+            35
         )
 
 
@@ -67,24 +61,21 @@ class EnemiesFactory:
             enemies_data["ants"]["intro_alive"],
             3,
             80,
-            40,
-            True
+            40
         )
         self.trog = Enemy(
             "Trog",
             enemies_data["trog"]["intro_alive"],
             3,
             90,
-            45,
-            True
+            45
         )
         self.uncanny = Enemy(
             "Uncanny Valley",
             enemies_data["uncanny"]["intro_alive"],
             4,
             100,
-            50,
-            True
+            50
         )
 
         # level 4
@@ -93,8 +84,7 @@ class EnemiesFactory:
             enemies_data["gnome"]["intro_alive"],
             4,
             120,
-            60,
-            True
+            60
         )
 
         # Level 5
@@ -103,7 +93,6 @@ class EnemiesFactory:
             enemies_data["ostoyae"]["intro_alive"],
             5,
             150,
-            75,
-            True
+            75
         )
 
