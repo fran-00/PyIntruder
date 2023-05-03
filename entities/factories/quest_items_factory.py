f645aa38cb9fa7e993744cca4df87ba56f97c680
fran-00 <fran.mf011@gmail.com>
2023-05-03 09:48:14 +0200

    Remove any description attribute from Item subclasses instances

--- entities/factories/quest_items_factory.py
+++ entities/factories/quest_items_factory.py
@@ -7,13 +7,11 @@ class QuestItemsFatory:
         
         self.specimen = Item(
             "The Specimen",
-            items_data["mission items"]["specimen"],
             1,
             10
         )
         self.bottle = Item(
             "bottle",
-            items_data["mission items"]["bottle"],
             1,
             10
         )
