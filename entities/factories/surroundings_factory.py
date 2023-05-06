0b7d6b9d852805c6f329862a57d7e3dc96ed82b8
fran-00 <fran.mf011@gmail.com>
2023-05-06 16:50:21 +0200

    Add SurroundingsFactory to entities.factories module

---
+++ entities/factories/surroundings_factory.py
@@ -0,0 +1,13 @@
+from ..entities_templates import Entity
+
+
+class SurroundingsFactory:
+    
+    def __init__(self):
+        
+        self.specimen = Entity(
+            "your car",
+        )
+        self.specimen = Entity(
+            "chest",
+        )
