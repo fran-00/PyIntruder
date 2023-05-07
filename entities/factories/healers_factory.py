063e73fa7c366ba3e237914d2052f2002b61a8ec
fran-00 <fran.mf011@gmail.com>
2023-05-07 16:41:26 +0200

    Remove value attribute from Healer instances

--- entities/factories/healers_factory.py
+++ entities/factories/healers_factory.py
@@ -7,43 +7,35 @@ class HealersFactory:
         
         self.ats = Healer(
             "Advanced Tea Substitute",
-            1,
-            3           
+            1,       
         )       
         self.gommo = Healer(
             "Gommo",
-            1,
-            3           
+            1,       
         )
         self.yellow_liquid = Healer(
             "Suspicious Yellow Liquid",
-            1,
-            3           
+            1,       
         )
         self.golden_apple = Healer(
             "Golden Apple",
-            1,
-            3           
+            1,       
         )
         self.nuka_cola = Healer(
             "Nuka Cola",
-            1,
-            3           
+            1,       
         )
         self.bandages = Healer(
             "Bandages",
-            1,
-            3           
+            1,       
         )
         self.gummy_bears = Healer(
             "Gummy Bears",
-            1,
-            3           
+            1,       
         )
         self.mushrooms = Healer(
             "Mushrooms",
-            1,
-            3           
+            1,       
         )
     
     def get_items_list(self):
