4cc9da992c6087fa69e3cc59c4f5757466cd4d97
fran-00 <fran.mf011@gmail.com>
2023-05-08 10:47:07 +0200

    Add ManaRechargersFactory to imports

--- entities/factories/__init__.py
+++ entities/factories/__init__.py
@@ -2,6 +2,7 @@ from .weapons_factory import WeaponsFactory as weapons
 from .curses_factory import CursesFactory as curses
 from .armors_factory import ArmorsFactory as armors
 from .healers_factory import HealersFactory as healers
+from .mana_rechargers_factory import ManaRechargersFactory as manarechargers
 from .mission_related_items_factory import MissionRelatedItemsFactory as missionitems
 from .surroundings_factory import SurroundingsFactory as surroundings
 from .npcs_factory import NPCsFactory as npcs
