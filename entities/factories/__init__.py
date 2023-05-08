93548be17838a512c33af480b9338cb465b6420a
fran-00 <fran.mf011@gmail.com>
2023-05-08 10:17:00 +0200

    Update __init__.py file to combine imports using package initialization in factories folder

---
+++ entities/factories/__init__.py
@@ -0,0 +1,7 @@
+from .weapons_factory import WeaponsFactory as weapons
+from .curses_factory import CursesFactory as curses
+from .armors_factory import ArmorsFactory as armors
+from .healers_factory import HealersFactory as healers
+from .mission_related_items_factory import MissionRelatedItemsFactory as missionitems
+from .surroundings_factory import SurroundingsFactory as surroundings
+from .npcs_factory import NPCsFactory as npcs
