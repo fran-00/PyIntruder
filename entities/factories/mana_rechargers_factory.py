404b6ad24a4174a13a91a5f1e5b2dabad8c988c6
fran-00 <fran.mf011@gmail.com>
2023-05-09 12:26:17 +0200

    Remove factories package

--- entities/factories/mana_rechargers_factory.py
+++
@@ -1,9 +0,0 @@
-from ..entities_templates import ManaRecharger
-
-class ManaRechargersFactory:
-    
-    def __init__(self):
-        self.name = ManaRecharger(
-            "name",
-            10
-        )
