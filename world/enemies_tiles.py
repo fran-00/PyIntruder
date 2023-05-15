84105da2e328bad605c19e14dd9d139e818b2065
fran-00 <fran.mf011@gmail.com>
2023-05-15 11:09:57 +0200

    Change class import and its usage from Factory to EntitiesFactory subclass and call it enemies to improve code readability

--- world/enemies_tiles.py
+++ world/enemies_tiles.py
@@ -1,7 +1,7 @@
 import random
 
 from .map_tile import MapTile
-from entities.factory import Factory
+from entities.factory import EntitiesFactory as enemies
 
 
 class EnemyTile_1(MapTile):
@@ -9,7 +9,7 @@ class EnemyTile_1(MapTile):
         self.name = 'Path'
         self.water = False
         super().__init__(x, y)
-        self.enemy_list = [Factory().gel_cube, Factory().squirrel, Factory().helicopter]
+        self.enemy_list = [enemies().gel_cube, enemies().squirrel, enemies().helicopter]
         self.enemy = random.choice(self.enemy_list)
 
 
@@ -17,7 +17,7 @@ class EnemyTile_2(MapTile):
     def __init__(self, x, y):
         self.name = 'Path'
         super().__init__(x, y)
-        self.enemy_list = [Factory().hunter, Factory().hunter, Factory().eyes]
+        self.enemy_list = [enemies().hunter, enemies().hunter, enemies().eyes]
         self.enemy = random.choice(self.enemy_list)
 
 
@@ -25,7 +25,7 @@ class EnemyTile_3(MapTile):
     def __init__(self, x, y):
         self.name = 'Path'
         super().__init__(x, y)
-        self.enemy_list = [Factory().ants, Factory().trog, Factory().uncanny]
+        self.enemy_list = [enemies().ants, enemies().trog, enemies().uncanny]
         self.enemy = random.choice(self.enemy_list)
 
 
@@ -33,7 +33,7 @@ class EnemyTile_4(MapTile):
     def __init__(self, x, y):
         self.name = 'Path'
         super().__init__(x, y)
-        self.enemy_list = [Factory().gnome]
+        self.enemy_list = [enemies().gnome]
         self.enemy = random.choice(self.enemy_list)
 
 
@@ -41,5 +41,5 @@ class EnemyTile_5(MapTile):
     def __init__(self, x, y):
         self.name = 'Path'
         super().__init__(x, y)
-        self.enemy_list = [Factory().ostoyae]
+        self.enemy_list = [enemies().ostoyae]
         self.enemy = random.choice(self.enemy_list)
