1d6db15d4560f5924cea113f7fd3817d42d9c416
fran-00 <fran.mf011@gmail.com>
2023-05-15 11:07:34 +0200

    Update Factory class by separating Items and Entities into two distinct subclasses: ItemsFactory and EntitiesFactory

--- entities/factory.py
+++ entities/factory.py
@@ -1,7 +1,18 @@
+import random
+
 from .templates import Armor, Curse, Enemy, Healer, ManaRecharger, MissionRelatedItem, NonPlayableCharacter, Surrounding, Weapon
 
 
 class Factory:
+
+    def get_entities_list(self, cls=None) -> list:
+        if cls is None:
+            return list(self.__dict__.values())
+        else:
+            return [entity for entity in self.__dict__.values() if isinstance(entity, cls)]
+
+
+class ItemsFactory(Factory):
 
     def __init__(self):
         self.rinas_armor = Armor("Rina's Armor", 10)
@@ -29,18 +40,6 @@ class Factory:
         self.briefcase = Curse("Briefcase", 90)
         self.riemann = Curse("Riemann zeta function", 95)
 
-        self.gel_cube = Enemy("Gelatinous Cube", 1, 20, 10)
-        self.squirrel = Enemy("Ravenous Squirrel", 1, 30, 15)
-        self.helicopter = Enemy("Helicopter", 1, 40, 20)
-        self.hunter = Enemy("Mushroom Hunter", 2, 50, 25)
-        self.bug = Enemy("Bug in the Code", 2, 60, 30)
-        self.eyes = Enemy("Floating Eyes", 3, 70, 35)
-        self.ants = Enemy("Ants infected by a Fungus", 3, 80, 40)
-        self.trog = Enemy("Trog", 3, 90, 45)
-        self.uncanny = Enemy("Uncanny Valley", 4, 100, 50)
-        self.gnome = Enemy("Gnome armed with an ax", 4, 120, 60)
-        self.ostoyae = Enemy("Armillaria Ostoyae", 5, 150, 75)
-
         self.ats = Healer("Advanced Tea Substitute", 10)
         self.gommo = Healer("Gommo", 20)
         self.yellow_liquid = Healer("Suspicious Yellow Liquid", 30)
@@ -60,24 +59,6 @@ class Factory:
         self.specimen = MissionRelatedItem("The Specimen")
         self.bottle = MissionRelatedItem("bottle", openable=True)
 
-        self.littleo = NonPlayableCharacter("Little(o)", [], True)
-        self.blacksmith = NonPlayableCharacter("Blacksmith", [], True)
-        self.innkeeper = NonPlayableCharacter("Innkeeper", [], True)
-        self.merchant = NonPlayableCharacter("Merchant", [], True)
-        self.monk = NonPlayableCharacter("Monk", [], True)
-        self.ferns = NonPlayableCharacter("Ferns", [], False)
-        self.rina = NonPlayableCharacter("Rina", [], False)
-        self.intruder = NonPlayableCharacter("Intruder", [], False)
-        self.oak = NonPlayableCharacter("Oak", [], False)
-        self.enzopaolo = NonPlayableCharacter("Enzo Paolo", [], False)
-        self.priamo = NonPlayableCharacter("Priamo", [], False)
-        self.effrafax = NonPlayableCharacter("Effrafax", [], False)
-        self.stylite = NonPlayableCharacter("Stylite", [], False)
-
-        self.car = Surrounding("car", [self.bottle], openable=True)
-        self.chest = Surrounding("chest", [], openable=True)
-        self.table = Surrounding("wooden table", [])
-
         self.manuport = Weapon("Manuport", 5)
         self.sheet = Weapon("polarized sheet", 10)
         self.polyhedron = Weapon("Sharp Polyhedron", 15)
@@ -93,8 +74,35 @@ class Factory:
         self.attractor = Weapon("Great Attractor", 65)
         self.cube = Weapon("The Soul Cube", 70)
 
-    def get_entities_list(self, cls=None) -> list:
-        if cls is None:
-            return list(self.__dict__.values())
-        else:
-            return [entity for entity in self.__dict__.values() if isinstance(entity, cls)]
+
+class EntitiesFactory(Factory):
+    def __init__(self):
+        self.gel_cube = Enemy("Gelatinous Cube", 1, 20, 10)
+        self.squirrel = Enemy("Ravenous Squirrel", 1, 30, 15)
+        self.helicopter = Enemy("Helicopter", 1, 40, 20)
+        self.hunter = Enemy("Mushroom Hunter", 2, 50, 25)
+        self.bug = Enemy("Bug in the Code", 2, 60, 30)
+        self.eyes = Enemy("Floating Eyes", 3, 70, 35)
+        self.ants = Enemy("Ants infected by a Fungus", 3, 80, 40)
+        self.trog = Enemy("Trog", 3, 90, 45)
+        self.uncanny = Enemy("Uncanny Valley", 4, 100, 50)
+        self.gnome = Enemy("Gnome armed with an ax", 4, 120, 60)
+        self.ostoyae = Enemy("Armillaria Ostoyae", 5, 150, 75)
+
+        self.littleo = NonPlayableCharacter("Little(o)", [], True)
+        self.blacksmith = NonPlayableCharacter("Blacksmith", [], True)
+        self.innkeeper = NonPlayableCharacter("Innkeeper", [], True)
+        self.merchant = NonPlayableCharacter("Merchant", [], True)
+        self.monk = NonPlayableCharacter("Monk", [], True)
+        self.ferns = NonPlayableCharacter("Ferns", [], False)
+        self.rina = NonPlayableCharacter("Rina", [], False)
+        self.intruder = NonPlayableCharacter("Intruder", [], False)
+        self.oak = NonPlayableCharacter("Oak", [], False)
+        self.enzopaolo = NonPlayableCharacter("Enzo Paolo", [], False)
+        self.priamo = NonPlayableCharacter("Priamo", [], False)
+        self.effrafax = NonPlayableCharacter("Effrafax", [], False)
+        self.stylite = NonPlayableCharacter("Stylite", [], False)
+
+        self.car = Surrounding("car", [ItemsFactory().bottle], openable=True)
+        self.chest = Surrounding("chest", [], openable=True)
+        self.table = Surrounding("wooden table", [])
