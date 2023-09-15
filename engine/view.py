1b1d4b4dde7c0fdf0c9b09f4d7e5116552570834
fran-00 <fran.mf011@gmail.com>
2023-09-15 18:28:16 +0200

    Add new module to manage GUI and create GameButtons class

--- engine/view.py
+++ engine/view.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QLineEdit, QPushButton
 from PyQt6.QtCore import pyqtSignal, pyqtSlot
 
+from gui.widgets import GameButtons
+
 
 class GameView(QWidget):
     view_signal_to_controller = pyqtSignal(str)
@@ -14,8 +16,8 @@ class GameView(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.on_log_view())
         layout.addLayout(self.on_input_layout())
-        layout.addLayout(self.on_movements_buttons())
-        layout.addLayout(self.on_actions_buttons())
+        layout.addLayout(GameButtons(self).on_movements_buttons())
+        layout.addLayout(GameButtons(self).on_actions_buttons())
         self.setLayout(layout)
 
     def on_log_view(self):
@@ -43,52 +45,6 @@ class GameView(QWidget):
         input_layout.addWidget(send_button)
 
         return input_layout
-
-    def on_movements_buttons(self):
-        """Add buttons for cardinal directions to game GUI"""
-        button_north = QPushButton("🡅")
-        button_south = QPushButton("🡇")
-        button_east = QPushButton("🡆")
-        button_west = QPushButton("🡄")
-
-        buttons = [button_north, button_south, button_east, button_west]
-        for button in buttons:
-            button.setProperty("class", "cardinal_directions")
-
-        # Connect direction buttons to a common handler
-        button_north.clicked.connect(lambda: self.handle_user_action("n"))
-        button_south.clicked.connect(lambda: self.handle_user_action("s"))
-        button_east.clicked.connect(lambda: self.handle_user_action("e"))
-        button_west.clicked.connect(lambda: self.handle_user_action("w"))
-
-        # Horizontal layout for direction buttons
-        direction_layout = QHBoxLayout()
-        direction_layout.addWidget(button_north)
-        direction_layout.addWidget(button_south)
-        direction_layout.addWidget(button_east)
-        direction_layout.addWidget(button_west)
-        
-        return direction_layout
-    
-    def on_actions_buttons(self):
-        """Add buttons for actions to game GUI"""
-        button_attack = QPushButton("Attack")
-        button_curse = QPushButton("Cast Curse")
-        button_inventory = QPushButton("Inventory")
-        button_diagnose = QPushButton("Diagnose")
-
-        button_attack.clicked.connect(lambda: self.handle_user_action("a"))
-        button_curse.clicked.connect(lambda: self.handle_user_action("c"))
-        button_inventory.clicked.connect(lambda: self.handle_user_action("i"))
-        button_diagnose.clicked.connect(lambda: self.handle_user_action("diagnose"))
-
-        command_layout = QHBoxLayout()
-        command_layout.addWidget(button_attack)
-        command_layout.addWidget(button_curse)
-        command_layout.addWidget(button_inventory)
-        command_layout.addWidget(button_diagnose)
-        
-        return command_layout
 
     def handle_user_action(self, command):
         if command == "none":
