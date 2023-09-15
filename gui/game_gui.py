183a857064cf465228f6dbd6da7902dfe64c5f50
fran-00 <fran.mf011@gmail.com>
2023-09-15 19:53:26 +0200

    Update assign_css_class variables to be more generic

--- gui/game_gui.py
+++ gui/game_gui.py
@@ -2,6 +2,6 @@ class GameGUI:
     def __init__(self, game_view):
         self.game_view = game_view
 
-    def assign_css_class(self, buttons_list, class_name):
-        for button in buttons_list:
-            button.setProperty("class", class_name)
+    def assign_css_class(self, gui_elements, class_name):
+        for element in gui_elements:
+            element.setProperty("class", class_name)
