861fda590805bfab3ff9ad33e8bd9be88bcef40f
fran-00 <fran.mf011@gmail.com>
2023-09-15 19:43:21 +0200

    Update GameMenu class adding add_menu_action and some WIP methods for future implementation

--- gui/menubar.py
+++ gui/menubar.py
@@ -3,11 +3,29 @@ from PyQt6.QtGui import QAction, QIcon
 class GameMenu:
     def __init__(self, game_view):
         self.game_view = game_view
+        self.menubar = self.game_view.menuBar()
 
-    def create_menu(self):
-        menubar = self.game_view.menuBar()
-        file_menu = menubar.addMenu("File")
+    def create_file_menu(self):
+        file_menu = self.menubar.addMenu("File")
+        self.add_menu_action(file_menu, self.new_game, "New Game")
+        self.add_menu_action(file_menu, self.on_save, "Save")
+        self.add_menu_action(file_menu, self.on_save_as, "Save As...")
+        self.add_menu_action(file_menu, self.on_reload, "Reload")
+        self.add_menu_action(file_menu, self.game_view.close, "Exit")
 
-        exit_action = QAction("Exit", self.game_view)
-        exit_action.triggered.connect(self.game_view.close)
-        file_menu.addAction(exit_action)
+    def add_menu_action(self, menu, action, text):
+        menu_action = QAction(text, self.game_view)
+        menu_action.triggered.connect(action)
+        menu.addAction(menu_action)
+
+    def new_game(self):
+        pass
+
+    def on_save(self):
+        pass
+
+    def on_save_as(self):
+        pass
+
+    def on_reload(self):
+        pass
