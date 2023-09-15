bfb246968aa2dbf6d255d18274251e74d706f957
fran-00 <fran.mf011@gmail.com>
2023-09-15 18:52:00 +0200

    Update GameButtons class improving on_movements_buttons and on_actions_buttons method removing repeated code

--- gui/widgets.py
+++ gui/widgets.py
@@ -6,52 +6,32 @@ class GameButtons:
     
     def on_movements_buttons(self):
         """Add buttons for cardinal directions to game GUI"""
-        button_north = QPushButton("🡅")
-        button_south = QPushButton("🡇")
-        button_east = QPushButton("🡆")
-        button_west = QPushButton("🡄")
+        buttons = []
+        directions_layout = QHBoxLayout()
 
-        buttons = [button_north, button_south, button_east, button_west]
+        self.add_button("🡅", "n", directions_layout, buttons)
+        self.add_button("🡇", "s", directions_layout, buttons)
+        self.add_button("🡆", "e", directions_layout, buttons)
+        self.add_button("🡄", "w", directions_layout, buttons)
+
         self.assign_css_class(buttons, "cardinal_directions")
 
-        # Connect direction buttons to a common handler
-        button_north.clicked.connect(lambda: self.game_view.handle_user_action("n"))
-        button_south.clicked.connect(lambda: self.game_view.handle_user_action("s"))
-        button_east.clicked.connect(lambda: self.game_view.handle_user_action("e"))
-        button_west.clicked.connect(lambda: self.game_view.handle_user_action("w"))
-
-        # Horizontal layout for direction buttons
-        direction_layout = QHBoxLayout()
-        direction_layout.addWidget(button_north)
-        direction_layout.addWidget(button_south)
-        direction_layout.addWidget(button_east)
-        direction_layout.addWidget(button_west)
-        
-        return direction_layout
+        return directions_layout
     
     def on_actions_buttons(self):
         """Add buttons for actions to game GUI"""
-        button_attack = QPushButton("Attack")
-        button_curse = QPushButton("Cast Curse")
-        button_inventory = QPushButton("Inventory")
-        button_diagnose = QPushButton("Diagnose")
+        buttons = []
+        actions_layout = QHBoxLayout()
 
-        buttons = [button_attack, button_curse, button_inventory, button_diagnose]
-        self.assign_css_class(buttons, "player_commands")
+        self.add_button("Attack", "a", actions_layout, buttons)
+        self.add_button("Cast Curse", "c", actions_layout, buttons)
+        self.add_button("Inventory", "i", actions_layout, buttons)
+        self.add_button("Diagnose", "diagnose", actions_layout, buttons)
 
-        button_attack.clicked.connect(lambda: self.game_view.handle_user_action("a"))
-        button_curse.clicked.connect(lambda: self.game_view.handle_user_action("c"))
-        button_inventory.clicked.connect(lambda: self.game_view.handle_user_action("i"))
-        button_diagnose.clicked.connect(lambda: self.game_view.handle_user_action("diagnose"))
+        self.assign_css_class(buttons, "action_buttons")
 
-        command_layout = QHBoxLayout()
-        command_layout.addWidget(button_attack)
-        command_layout.addWidget(button_curse)
-        command_layout.addWidget(button_inventory)
-        command_layout.addWidget(button_diagnose)
-        
-        return command_layout
-    
+        return actions_layout
+
     def add_button(self, text, command, layout, buttons_list):
         button = QPushButton(text)
         button.clicked.connect(lambda: self.game_view.handle_user_action(command))
