d483f02919e33613fed3f7f3e762516ece17f641
fran-00 <fran.mf011@gmail.com>
2023-09-16 15:00:29 +0200

    Update colors with hex codes

--- styles/decorators.py
+++ styles/decorators.py
@@ -1,25 +1,25 @@
 def white(func):
     def wrapper(*args, **kwargs):
         string = func(*args, **kwargs)
-        return f"<span style='color: white;'>{string}</span>"
+        return f"<span style='color: #FFFFFF;'>{string}</span>"
     return wrapper
 
 def red(func):
     def wrapper(*args, **kwargs):
         string = func(*args, **kwargs)
-        return f"<span style='color: red;'>{string}</span>"
+        return f"<span style='color: #FF0000;'>{string}</span>"
     return wrapper
 
 def green(func):
     def wrapper(*args, **kwargs):
         string = func(*args, **kwargs)
-        return f"<span style='color: green;'>{string}</span>"
+        return f"<span style='color: #00FF00;'>{string}</span>"
     return wrapper
 
 def blue(func):
     def wrapper(*args, **kwargs):
         string = func(*args, **kwargs)
-        return f"<span style='color: blue;'>{string}</span>"
+        return f"<span style='color: #0000FF;'>{string}</span>"
     return wrapper
 
 def underline(func):
