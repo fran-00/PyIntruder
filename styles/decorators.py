757e37f85837b7da34476b1e6fefaf36278e1a71
fran-00 <fran.mf011@gmail.com>
2023-09-16 14:49:45 +0200

    Add decorators to apply inline css style to formatted strings text

---
+++ styles/decorators.py
@@ -0,0 +1,29 @@
+def red(func):
+    def wrapper(*args, **kwargs):
+        string = func(*args, **kwargs)
+        return f"<span style='color: red;'>{string}</span>"
+    return wrapper
+
+def green(func):
+    def wrapper(*args, **kwargs):
+        string = func(*args, **kwargs)
+        return f"<span style='color: green;'>{string}</span>"
+    return wrapper
+
+def blue(func):
+    def wrapper(*args, **kwargs):
+        string = func(*args, **kwargs)
+        return f"<span style='color: blue;'>{string}</span>"
+    return wrapper
+
+def underline(func):
+    def wrapper(*args, **kwargs):
+        string = func(*args, **kwargs)
+        return f"<span style='text-decoration: underline;'>{string}</span>"
+    return wrapper
+
+def bold(func):
+    def wrapper(*args, **kwargs):
+        string = func(*args, **kwargs)
+        return f"<span style='font-weight: bold;'>{string}</span>"
+    return wrapper
