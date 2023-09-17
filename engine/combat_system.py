7a147bc035a968ca1c4b256c943f9152aee692bd
fran-00 <fran.mf011@gmail.com>
2023-09-17 16:23:24 +0200

    Add new Combat class to handle combat system to game engine

---
+++ engine/combat_system.py
@@ -0,0 +1,11 @@
+import random
+from dataclasses import dataclass
+
+from entities.templates import Armor, Curse, Healer, ManaRecharger, MissionRelatedItem, Weapon
+import world.parser as parser
+from styles.decorators import *
+
+
+@dataclass
+class Combat:
+    pass
