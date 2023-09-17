2fdf8bdda74bf6a194bd016570de2b02215a6a05
fran-00 <fran.mf011@gmail.com>
2023-09-17 11:41:03 +0200

    Add crete_mana_bar method to GameProgressBars

--- gui/status_bars.py
+++ gui/status_bars.py
@@ -4,10 +4,14 @@ from .game_gui import GameGUI
 from entities.player import Player
 
 
-class HealthBar(GameGUI):
+class GameProgressBars(GameGUI):
     def __init__(self, game_view):
         super().__init__(game_view)
 
     def crete_health_bar(self):
         self.health_bar = QProgressBar(self.game_view, objectName="health_bar")
         return self.health_bar
+
+    def crete_mana_bar(self):
+        self.mana_bar = QProgressBar(self.game_view, objectName="mana_bar")
+        return self.mana_bar
